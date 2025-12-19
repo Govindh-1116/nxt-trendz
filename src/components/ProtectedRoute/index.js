cfb128ea35e36ada56b89d5250f8b5d520014a50
pediredlaganesh7 <ganeshpediredla123@gmail.com>
2025-12-19 21:39:00 +0530

    index.js

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,6 +1,7 @@
 import {Redirect, Route} from 'react-router-dom'
 import Cookie from 'js-cookie'
 
+
 const ProtectedRoute = props => {
   const token = Cookie.get('jwt_token')
   if (token === undefined) {
